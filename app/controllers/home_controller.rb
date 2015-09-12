d79a84529101affa0d656023b25a502137fd3ca5
Lee Ho Yean <ghdusjang@naver.com>
2015-09-12 07:16:43 +0000

    db seed & apply html(DB)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @sg = SmallGoal.all
   end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,12 @@ BigGoal.create(content: '사업')
 BigGoal.create(content: '가정')
 
 SmallGoal.create(title: '사업 구상하기',due: Date.new(2017,1,1), public_range: 0)
+SmallGoal.create(title: '공부하기',due: Date.new(2018,1,1), public_range: 0)
+SmallGoal.create(title: '잘 놀기',due: Date.new(2019,1,1), public_range: 0)
+
+Doplan.create(small_goal_id: 1, content: '사업 1', check: true)
+Doplan.create(small_goal_id: 1, content: '사업 2', check: false)
+Doplan.create(small_goal_id: 2, content: '공부 1', check: true)
+Doplan.create(small_goal_id: 2, content: '공부 2', check: false)
+Doplan.create(small_goal_id: 3, content: '놀기 1', check: true)
+Doplan.create(small_goal_id: 3, content: '놀기 2', check: false)
